cb5277d4f22ab3a96d3242a9a8a430a9a3a5437d
Pol Español Roca <pol8891@gmail.com>
2025-03-19 11:15:05 +0100

    last commit

--- test/components/GifGrid.test.jsx
+++ test/components/GifGrid.test.jsx
@@ -1,12 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import { GifGrid } from "../../src/components";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
 
+jest.mock('../../src/hooks/useFetchGifs');
 
 describe('pruebas en <GifGrid />', () => { 
 
     const category = 'One Punch';
 
     test('debe de mostrar el loading inicialmente', () => {
+
+        useFetchGifs.mockReturnValue({
+            images: [],
+            isLoading: true
+        });
 
         render( <GifGrid category={ category }/>);
         expect( screen.getByText( 'Cargando...' ) );
@@ -16,6 +23,26 @@ describe('pruebas en <GifGrid />', () => {
 
     test('dede mostrar items cuando se cargan las imagenes mediante useFetchGifs', () => { 
         
+
+        const gifs = [
+            {
+                id: 'ABC',
+                title: 'Saitama',
+                url: 'https://saitama.com'
+            },{
+                id: '123',
+                title: 'Goku',
+                url: 'https://goku.com'
+            }
+        ]
+        useFetchGifs.mockReturnValue({
+            images: gifs,
+            isLoading: false
+        });
+
+        render( <GifGrid category={ category }/>);
+        expect( screen.getAllByRole('img').length ).toBe(2);
+
 
 
      })
